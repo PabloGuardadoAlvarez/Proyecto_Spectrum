d96b41a857e26b1f8ce53e7c8d0f6eb5aebd4ab2
Pablo <pablo.guardado@live.u-tad.com>
2018-10-15 12:39:23 +0200

    Date Picker medio funcionando

--- app/src/main/java/io/github/alvarosanzrodrigo/intents/UserActivity.kt
+++ app/src/main/java/io/github/alvarosanzrodrigo/intents/UserActivity.kt
@@ -2,12 +2,34 @@ package io.github.alvarosanzrodrigo.intents
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.app.DatePickerDialog
+import android.support.v4.app.FragmentActivity
+import android.view.View
+import android.widget.DatePicker
+import android.widget.TextView
+
 
 class UserActivity : AppCompatActivity() {
+
+
+    lateinit var etxtDate:TextView
+    var TAG:String = "datePicker"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
         setResult(0)
     }
+
+    fun showDatePickerDialog(v: View) {
+        val newFragment = DatePickerFragment.newInstance(object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(datePicker: DatePicker, year: Int, month: Int, day: Int) {
+                // +1 because january is zero
+                val selectedDate = day.toString() + " / " + (month + 1) + " / " + year
+                etxtDate.setText(selectedDate)
+            }
+        })
+        newFragment.show(supportFragmentManager, TAG)
+    }
+
 }
